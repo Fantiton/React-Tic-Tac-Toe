50be54bfb784ed4d4d55a0023d8bb83a785fba99
Filip Celejewski <FilipCelejewski@wp.pl>
2025-06-08 14:54:26 +0200

    Moves History Refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import { useState} from 'react'
 
-export default function Board() {
+export default function Game(){
   const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length - 1]
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares])
+    setXIsNext(!xIsNext)
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol></ol>
+      </div>
+    </div>
+  )
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+
   const winner = CalculateWinner(squares)
 
   let status 
@@ -15,8 +36,7 @@ export default function Board() {
 
     (xIsNext) ? nextSquares[i] = "X" : nextSquares[i] = "O"
 
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares);
   }
 
   return <>
